b6dc7bd5892e9b3585b3319f6bb31b285db3d66c
nvelthorst <nvelthorst@gmail.com>
2020-07-08 15:43:15 +0200

    Moved composer.json

--- src/TasksServiceProvider.php
+++ src/TasksServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace Label305\Tasks;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use Label305\Tasks\Support\TaskResult;
+use Label305\Tasks\Support\TaskState;
+use Label305\Tasks\Logging\LogSession;
 
 class TasksServiceProvider extends ServiceProvider
 {
@@ -24,5 +28,12 @@ class TasksServiceProvider extends ServiceProvider
     public function boot()
     {
         include __DIR__ . '/routes.php';
+
+        $this->app->singleton('LogSession', function () {
+            return new LogSession(Storage::disk(config('filesystems.cloud')));
+        });
+
+        $this->app->singleton('TaskResult', TaskResult::class);
+        $this->app->singleton('TaskState', TaskState::class);
     }
 }
