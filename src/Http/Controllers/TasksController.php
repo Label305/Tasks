0470b3394072dfeba7bffc52597c00ba4d88456d
nvelthorst <nvelthorst@gmail.com>
2020-07-09 13:39:36 +0200

    Added TasksController

---
+++ src/Http/Controllers/TasksController.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Label305\Tasks\Http\Controllers;
+
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Storage;
+use Label305\Tasks\Persistence\Tasks\TaskRepository;
+use Label305\Tasks\DispatchesTasks;
+use Label305\Tasks\Enums\State;
+
+class TasksController extends Controller
+{
+
+    use DispatchesTasks;
+
+    public function log(int $id, TaskRepository $taskRepository)
+    {
+        $task = $taskRepository->findById($id);
+
+        if ($task === null) {
+            abort(404);
+        }
+
+        if ($task->getState() !== State::FINISHED) {
+            if (!file_exists($task->getLocalPathForLog())) {
+                return response('No log found.. yet.', 200, ['Content-Type' => 'text/plain']);
+            }
+
+            return response()->file($task->getLocalPathForLog());
+        }
+
+        $cloudDisk = Storage::disk(config('filesystems.cloud'));
+        if (!$cloudDisk->exists($task->getPersistentPathForLog())) {
+            return response('No log found in cloud.. yet.', 200, ['Content-Type' => 'text/plain']);
+        }
+
+        $url = $cloudDisk->url($task->getPersistentPathForLog());
+        return redirect()->to($url);
+    }
+
+    public function finish(int $taskId, TaskRepository $taskRepository){
+
+        $task = $taskRepository->findById($taskId);
+        $task->setState(State::FINISHED);
+        $taskRepository->store($task);
+
+        return redirect()->route('tasks.show', ['id' => $task->getId()]);
+    }
+
+    public function longRunning(int $taskId, TaskRepository $taskRepository)
+    {
+        $task = $taskRepository->findById($taskId);
+        $task->setIsLongRunning(true);
+        $taskRepository->store($task);
+
+        return redirect()->back();
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,35 +1,20 @@
 <?php
 
-Route::get('/tasks', [
-    'as' => 'tasks.index',
-    'uses' => 'TasksController@index'
-]);
-
 Route::post('/tasks/export', [
     'as' => 'tasks.export',
-    'uses' => 'TasksController@export',
+    'uses' => 'Label305\\Tasks\\Http\\Controllers\\TasksController@export',
 ]);
 
 Route::post('/tasks/{id}/finish', [
     'as' => 'tasks.finish',
-    'uses' => 'TasksController@finish',
+    'uses' => 'Label305\\Tasks\\Http\\Controllers\\TasksController@finish',
 ]);
 Route::post('/tasks/{id}/long-running', [
     'as' => 'tasks.is_long_running',
-    'uses' => 'TasksController@longRunning'
-]);
-
-Route::get('/tasks/{id}', [
-    'as' => 'tasks.show',
-    'uses' => 'TasksController@show'
+    'uses' => 'Label305\\Tasks\\Http\\Controllers\\TasksController@longRunning'
 ]);
 
 Route::get('/tasks/{id}/log', [
     'as' => 'tasks.log',
-    'uses' => 'TasksController@log'
-]);
-
-Route::get('/tasks/ping-school/{id}', [
-    'as' => 'tasks.ping',
-    'uses' => 'TasksController@ping'
+    'uses' => 'Label305\\Tasks\\Http\\Controllers\\TasksController@log'
 ]);
