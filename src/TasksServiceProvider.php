d56721bade40673df7332096a502b80139723c15
nvelthorst <nvelthorst@gmail.com>
2020-09-24 15:44:25 +0200

    Moved TaskRepository binding

--- src/TasksServiceProvider.php
+++ src/TasksServiceProvider.php
@@ -4,9 +4,11 @@ namespace Label305\Tasks;
 
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use Label305\Tasks\Logging\LogSession;
+use Label305\Tasks\Persistence\Tasks\EloquentTaskRepository;
+use Label305\Tasks\Persistence\Tasks\TaskRepository;
 use Label305\Tasks\Support\TaskResult;
 use Label305\Tasks\Support\TaskState;
-use Label305\Tasks\Logging\LogSession;
 
 class TasksServiceProvider extends ServiceProvider
 {
@@ -37,5 +39,7 @@ class TasksServiceProvider extends ServiceProvider
 
         $this->app->singleton('TaskResult', TaskResult::class);
         $this->app->singleton('TaskState', TaskState::class);
+
+        $this->app->bind(TaskRepository::class, EloquentTaskRepository::class);
     }
 }
